ecbfc8f1a5da0369a240a7146cec31f11b6be5f0
Saalik Lokhandwala <saalikl111@gmail.com>
2021-11-07 06:33:23 -0600

    create and destroy website api

--- spec/controller/api/v1/websites_controller_spec.rb
+++ spec/controller/api/v1/websites_controller_spec.rb
@@ -30,24 +30,73 @@ describe Api::V1::WebsitesController, type: :controller do
   end
 
   describe "POST#create" do
-    xit "creates a new website" do
+    it "creates a new website" do
+      post_json = {
+        title: "A Website",
+        url: "website.com"
+      }
+
+      prev_count = Website.count
+      post(:create, params: post_json, format: :json)
+
+      expect(response.status).to eq 200
+      expect(Website.count).to eq(prev_count + 1)
     end
 
-    xit "returns website data" do
+    it "returns website data" do
+      post_json = {
+        title: "A Website",
+        url: "website.com"
+      }
+
+      post(:create, params: post_json, format: :json)
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(returned_json["title"]).to eq("A Website")
+      expect(returned_json["url"]).to eq("website.com")
     end
 
-    xit "fails to create website with too long a title" do
+    it "fails to create website with too long a title" do
+      post_json = {
+        title: "This is a website title and this is how big it is over",
+        url: "website.com"
+      }
+
+      post(:create, params: post_json, format: :json)
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 422
+      expect(returned_json["errors"]["title"][0]).to eq("Title is too long (maximum is 50 characters)")
     end
 
-    xit "fails to create a website without a valid url" do
+    it "fails to create a website without a valid url" do
+      post_json = {
+        title: "Website",
+        url: ""
+      }
+
+      post(:create, params: post_json, format: :json)
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 422
+      expect(returned_json["errors"]["url"][0]).to eq("Url can't be blank")
     end
   end
 
   describe "DELETE#destroy" do
-    xit "deletes website" do
+    let!(:website) { create(:website) }
+
+    it "deletes and returns website" do
+      delete :destroy, params: { id: website.id }
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(returned_json["title"]).to eq(website.title)
+      expect(returned_json["url"]).to eq(website.url)
     end
 
-    xit "fails to delete website with bad params" do
+    xit "deletes all associated visits" do
     end
   end
 end
